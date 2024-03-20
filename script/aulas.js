7a25ab27b208962696475f1db18cd1e39bff6f1a
Kamila Ingrid De Carvalho <kamilaingridc@gmail.com>
2024-03-19 22:30:16 -0300

    ...

---
+++ script/aulas.js
@@ -0,0 +1,4 @@
+function startLesson() {
+    alert("A aula de JavaScript começou! Vamos mergulhar nos conceitos e práticas desta linguagem incrível.");
+    document.getElementById('lessonContent').style.display = 'block';
+}
